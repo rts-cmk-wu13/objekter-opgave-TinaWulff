aa5f0bc0344a157bcdefe5ce27911e59f7e1f753
Tina Wulff <tina@tinawulff.dk>
2025-02-04 11:07:13 +0100

    opgave start header-Created

--- js/index.js
+++ js/index.js
@@ -2,3 +2,16 @@
 
 //eksempel på at udskrive alle overskrifter i services i konsollen:
 services.forEach(service => console.log(service.headline))
+
+
+
+// HEADER
+let header = document.createElement("img");
+header.src = hero.image;
+document.body.append(header);
+header.style.maxWidth = "1440px";
+header.style.display = "block"
+header.style.margin = "0 auto";
+
+let menuBar = document.createElement("nav");
+
