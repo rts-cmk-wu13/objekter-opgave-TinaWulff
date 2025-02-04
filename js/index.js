62e4a10fce050c7c678a0822bab75bc61b3ce318
Tina Wulff <tina@tinawulff.dk>
2025-02-04 11:34:12 +0100

    menu tilføjet

--- js/index.js
+++ js/index.js
@@ -5,7 +5,8 @@ services.forEach(service => console.log(service.headline))
 
 
 
-// HEADER
+/////////// HEADER
+//IMAGE
 let header = document.createElement("img");
 header.src = hero.image;
 document.body.append(header);
@@ -13,5 +14,14 @@ header.style.maxWidth = "1440px";
 header.style.display = "block"
 header.style.margin = "0 auto";
 
+///// MENU
+// NAV
 let menuBar = document.createElement("nav");
+
+let icon = document.createElement("img");
+icon.src = hero.icon;
+
+let buttonMenu = document.createElement("button");
+buttonMenu.innerHTML = '<i class="fa-solid fa-chevron-down"></i> <i class="fa-solid fa-user"></i>';
+
 
